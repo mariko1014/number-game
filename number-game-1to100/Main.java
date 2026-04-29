98923fd7f1e1be28eabfbd004449ef8d98ff47a6
mariko1014 <g2442085@tcu.ac.jp>
2026-04-30 04:27:19 +0900

    リトライ機能追加

--- number-game-1to100/Main.java
+++ number-game-1to100/Main.java
@@ -6,44 +6,60 @@ public class Main {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        int answer = rand.nextInt(100) + 1; // 1〜100
-        int guess = 0;
-        int count = 0;
-        int maxAttempts = 8; //回数制限
+        String retry = "y" ;
 
-        System.out.println("数当てゲーム！1〜100の数字を当ててね");
-        System.out.println(+ maxAttempts + "回以内に数字を当てたら成功"); //回数制限機能をつける時に入れる文
+        while (retry.equals( "y" )){
 
-        while (guess != answer) {
-            count++;
-            if(count < maxAttempts){
-                System.out.println("現在の回数: " + count);
-            }else if (count == maxAttempts) {
-                System.out.println("現在の回数: " + count + "　ラストチャンス！");
+
+            int answer = rand.nextInt(100) + 1; // 1〜100
+            int guess = 0;
+            int count = 0;
+            int maxAttempts = 5; //回数制限
+
+            System.out.println("数当てゲーム！1〜100の数字を当ててね");
+            System.out.println(+ maxAttempts + "回以内に数字を当てたら成功"); //回数制限機能をつける時に入れる文
+
+            while (guess != answer) {
+                count++;
+                if(count < maxAttempts){
+                    System.out.println("現在の回数: " + count);
+                }else if (count == maxAttempts) {
+                    System.out.println("現在の回数: " + count + "　ラストチャンス！");
+                }
+
+
+                System.out.print("予想を入力: ");
+                guess = scanner.nextInt();
+
+                if (guess < answer && count < maxAttempts) {
+                    System.out.println("もっと大きい！");
+                } else if (guess > answer && count < maxAttempts) {
+                    System.out.println("もっと小さい！");
+                } else if (guess == answer){
+                    System.out.println("正解！ " + count + "回で当たりました！");
+                }
+
+                if (count >= maxAttempts){ //解答回数が制限と同じ値になったらwhileから抜け出す
+                    break;
+                }
             }
 
-
-            System.out.print("予想を入力: ");
-            guess = scanner.nextInt();
-
-            if (guess < answer && count < maxAttempts) {
-                System.out.println("もっと大きい！");
-            } else if (guess > answer && count < maxAttempts) {
-                System.out.println("もっと小さい！");
-            } else if (guess == answer){
-                System.out.println("正解！ " + count + "回で当たりました！");
+            if(guess != answer){
+                System.out.println("残念！正解は　" + answer + "でした！");
             }
+            //抜け出した先で答えと入力値が違った時だけ、正解の値を表示。
+            
+            
+            scanner.nextLine();     //前回scannerで残っている改行を消費
 
-            if (count == maxAttempts){ //解答回数が制限と同じ値になったらwhileから抜け出す
-                break;
-            }
+            System.out.println("もう一回プレイしますか？");
+            System.out.print("はい→y　いいえ→n　: ");
+            retry = scanner.nextLine();
+
+
         }
-
-        if(guess != answer){
-            System.out.println("残念！正解は　" + answer + "でした！");
-        }
-        //抜け出した先で答えと入力値が違った時だけ、正解の値を表示。
-
         scanner.close();
+        System.out.println("またプレイしてね！");
+        
     }
 }
